08ac3b3d9557874a2d15b3d979cd6c165fa23f9a
tylercomisky <tdcomisky@gmail.com>
2019-11-09 10:28:59 -0500

    created topics arrays

---
+++ src/quidProQuo/Topics.java
@@ -0,0 +1,10 @@
+package quidProQuo;
+
+public class Topics {
+    public Decision[] year1Major = new Decision[10];
+    public Decision[] year1Minor = new Decision[10];
+    public Decision[] year2Major = new Decision[10];
+    public Decision[] year2Minor = new Decision[10];
+    public Decision[] year3Major = new Decision[10];
+    public Decision[] year3Minor = new Decision[10];
+}
